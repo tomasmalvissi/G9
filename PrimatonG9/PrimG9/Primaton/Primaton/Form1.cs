26e4943ba048ba428c7152adcd1b5e8d85fcbfc9
FranciscoCabreraLencina <54382771+FranciscoCabreraLencina@users.noreply.github.com>
2019-09-06 12:50:30 -0300

    Add files via upload

---
+++ PrimatonG9/PrimG9/Primaton/Primaton/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace Primaton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //palabras reservadas de "using System.Runtime.InteropServices para el movimiento de la barra de tareas
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void RealeaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnrestaurar.Visible = false;
+        }
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            if (txtNombre.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un nombre");
+                return;
+            }
+            if (txtApellido.Text == "")
+            {
+                MessageBox.Show("Debe ingresar un apellido");
+                return;
+            }
+            if (txtEscuela.Text == "")
+            {
+                MessageBox.Show("Debe ingresar una Escuela");
+                return;
+            }
+        }
+
+        private void btncerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnminimizar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Maximized;
+            btnmaximizar.Visible = false;
+            btnrestaurar.Visible = true;
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            RealeaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void restaurar_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            btnrestaurar.Visible = false;
+            btnmaximizar.Visible = true;
+        }
+    }
+}
